7e7c35f74a7fa9f7c56145d32610888acfb2cecd
Cebo Khumalo <cebolempumelelok@boxfusion.co.za>
2023-06-12 10:50:56 +0200

    Initial Commit

---
+++ src/pages/loginPage/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const loginPage = () => {
+    return <div>index</div>;
+};
+
+export default loginPage;
---
+++ src/pages/userPage/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const userPage = () => {
+    return <div>index</div>;
+};
+
+export default userPage;
